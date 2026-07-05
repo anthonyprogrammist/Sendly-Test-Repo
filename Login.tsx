32a9cf16d1c0349baf4d451c971cbdf5d3439d19
Hazyshades <30349843+Hazyshades@users.noreply.github.com>
2026-07-05 09:20:35 +0300

    Create Login.tsx

---
+++ Login.tsx
@@ -0,0 +1,23 @@
+import React, { useState } } from 'react';
+
+export default function BadComponent() {
+  const [user, setUser] = useState<User>(); 
+
+  const handleLog = () => {
+    console.log(text); 
+    
+    try {
+      let data = JSON.parse("не-json строка");
+    } 
+    catch (err: Error) { 
+      console.error(err.message);
+    }
+  };
+
+  if (user) {
+    return (
+      <h1>Привет, {user.name}</h1>
+      <button onClick={handleLog()}>Клик</button> 
+    );
+  }
+}
